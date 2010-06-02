9d2468b233615667dd454845cc426233a20dc183
Toni Spets <toni.spets@iki.fi>
2010-06-02 18:52:45 +0300

    made jslint default options compliant

--- cms/js/CMS.js
+++ cms/js/CMS.js
@@ -1,180 +1,5 @@
-$().ready(function() {
-
-    $('#cms_login').dialog({
-        autoOpen : false,
-        title : "Login",
-        buttons : {
-            "Go" : CMS.login
-        },
-        draggable : false,
-        resizable : false,
-        modal : true,
-        dialogClass : 'cms_login',
-        width : 250,
-        open : function(ev, ui) {
-            CMS.hideUI();
-            $(this).children('input').attr('value', '');
-            $(this).children('.focus').focus();
-        },
-    });
-    $('#cms_login input').keyup(function(ev) {
-        if(ev.keyCode == 13)
-            CMS.login();
-    });
-
-    $('#cms_message').dialog({
-        autoOpen : false,
-        title : "Working...",
-        draggable : false,
-        resizable : false,
-        modal : true,
-        dialogClass : 'cms_message',
-        width: 200,
-        height: 100,
-    });
-
-    $('#cms_tools').dialog({
-        autoOpen : false,
-        title : "Management",
-        resizable : false,
-        width : 'auto',
-        height : 'auto',
-        position : [ 20, 20 ],
-        dialogClass : 'cms_tools',
-        open : function(ev, ui) {
-            $('.cms_tools').trigger('mouseout');
-        },
-    });
-    $('.cms_tools').mouseover(function(ev) { $(this).fadeTo(0, 1); });
-    $('.cms_tools').mouseout(function(ev) { $(this).fadeTo(0, 0.7); });
-
-    $('#cms_page_new').dialog({
-        autoOpen : false,
-        title : "New page",
-        resizable : false,
-        width : 400,
-        height : 'auto',
-        buttons : {
-            "Cancel" : function(ev, ui) { $('#cms_page_new').dialog('close'); },
-            "Create" : function(ev, ui) {
-                var title = $('#cms_page_new input[name=title]').attr('value');
-                var template = $('#cms_page_new select[name=template]').val();
-                var parent_id = CMS.currentPage ? CMS.currentPage : null;
-
-                if(title.length == 0) {
-                    $('#cms_page_new .error').html("Title can't be empty.");
-                    $('#cms_page_new .ui-state-error').show();
-                    $('#cms_page_new .focus').focus();
-                    return;
-                } else if(template.length == 0) {
-                    $('#cms_page_new .error').html("You must select a template.");
-                    $('#cms_page_new .ui-state-error').show();
-                    $('#cms_page_new .focus').focus();
-                    return;
-                }
-
-                $('#cms_page_new').dialog('close');
-                CMS.query('page_new', { parent_id : parent_id, title : title, template : template }, CMS.updateTree);
-            },
-        },
-        open : function() {
-            var parent = CMS.currentPage;
-            if(parent == 0)
-                parent = '<em>root</em>';
-            $('#cms_page_new span.parent').html(parent);
-            $(this).children('input[name=title]').attr('value', '');
-            $(this).children('.ui-state-error').hide();
-            $(this).children('.focus').focus();
-        },
-    });
-
-    $('#cms_label').dialog({
-        autoOpen : false,
-        title : "Label Editor",
-        resizable : true,
-        width : 400,
-        minHeight : 110,
-        height : 'auto',
-        modal : true,
-        dialogClass : 'cms_label',
-        buttons : {
-            "Cancel" : function(ev, ui) { $('#cms_label').dialog('close'); },
-            "Save" : function(ev, ui) {
-                $('#cms_label').dialog('close');
-                CMS.save($('#cms_label input[name=page_id]').attr('value'), $('#cms_label input[name=field]').attr('value'), $('#cms_label input[name=value]').attr('value'));
-            },
-        },
-        open : function() {
-            $(this).children('input[name=value]').focus();
-        },
-    });
-    $('#cms_label input[name=value]').keyup(function(ev) {
-        if(ev.keyCode == 13) {
-            $('#cms_label').dialog('close');
-            CMS.save($('#cms_label input[name=page_id]').attr('value'), $('#cms_label input[name=field]').attr('value'), $('#cms_label input[name=value]').attr('value'));
-        }
-    });
-
-    $('#cms_tinymce').dialog({
-        autoOpen : false,
-        title : "HTML Editor",
-        resizable : false,
-        width : 'auto',
-        height : 'auto',
-        modal : true,
-        minWidth : 800,
-        minHeight : 500,
-        dialogClass : 'cms_tinymce',
-        zIndex : 1,
-        buttons : {
-            "Cancel" : function(ev, ui) { $('#cms_tinymce').dialog('close'); },
-            "Save" : function(ev, ui) {
-                $('#cms_tinymce').dialog('close');
-                CMS.save($('#cms_tinymce input[name=page_id]').attr('value'), $('#cms_tinymce input[name=field]').attr('value'), $('#cms_tinymce textarea[name=value]').attr('value'));
-            },
-        },
-        open : function() {
-            $('#cms_tinymce textarea').tinymce({
-                    // Location of TinyMCE script
-                    script_url : 'js/tinymce/jscripts/tiny_mce/tiny_mce.js',
-
-                    // General options
-                    theme : "advanced",
-                    plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
-                    width: '800px',
-                    height: '450px',
-
-                    // Theme options
-                    theme_advanced_buttons1 : "newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
-                    theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-                    theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-                    theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
-                    theme_advanced_toolbar_location : "top",
-                    theme_advanced_toolbar_align : "left",
-                    theme_advanced_statusbar_location : "bottom",
-                    theme_advanced_resizing : true
-            });
-        },
-        close : function() {
-            $('#cms_tinymce textarea').tinymce().remove();
-        },
-    });
-
-    $('#cms_page iframe').load(function() {
-        var uri = $('#cms_page iframe')[0].contentWindow.location.href;
-        if(uri) {
-            var m = uri.match(/id=(\d+)$/);
-            if(m) {
-                CMS.pageOpen(m[1], true);
-            }
-        }
-    });
-
-    CMS.checkSession();
-});
-
-var CMS = new Object();
-{
+var CMS = {};
+/*{*/
     CMS.currentPage = 0;
 
     CMS.test = function() {
@@ -191,17 +16,22 @@ var CMS = new Object();
             var pages = data.d;
             var str = '<ul class="root">';
             var open = false;
-            if(CMS.currentPage == 0)
+            if(CMS.currentPage === 0) {
                 open = true;
+            }
             for(var i in pages) {
-                var page = pages[i];
-                if(page.parent_id == null)
-                    CMS.currentPage = page.id;
-                str += CMS.addTreeNode(page);
+                if(pages.hasOwnProperty(i)) {
+                    var page = pages[i];
+                    if(page.parent_id === null) {
+                        CMS.currentPage = page.id;
+                    }
+                    str += CMS.addTreeNode(page);
+                }
             }
             $('#cms_tree').html(str+"</ul>");
-            if(open && CMS.currentPage != 0)
+            if(open && CMS.currentPage !== 0) {
                 CMS.pageOpen(CMS.currentPage);
+            }
         }
     };
 
@@ -219,11 +49,13 @@ var CMS = new Object();
         if(page.children.length > 0) {
             str += '<ul>';
             for(var i in page.children) {
-                str += CMS.addTreeNode(page.children[i]);
+                if(page.children.hasOwnProperty(i)) {
+                    str += CMS.addTreeNode(page.children[i]);
+                }
             }
             str += '</ul>';
         }
-        return str += '</li>';
+        return (str += '</li>');
     };
 
     CMS.hideUI = function() {
@@ -243,9 +75,9 @@ var CMS = new Object();
 
     CMS.checkSessionCallback = function(success, data)
     {
-        if(success && data.r == 'ok')
+        if(success && data.r == 'ok') {
             CMS.showUI();
-        else {
+        } else {
             $('#cms_login').dialog('open');
         }
     };
@@ -255,7 +87,7 @@ var CMS = new Object();
         var username = $('#cms_login input[name=username]').attr('value');
         var password = $('#cms_login input[name=password]').attr('value');
 
-        if(username.length == 0) {
+        if(username.length === 0) {
             $('#cms_login .error').html('Please input a username.');
             $('#cms_login .ui-state-error').show();
             $('#cms_login .focus').focus();
@@ -268,9 +100,9 @@ var CMS = new Object();
 
     CMS.loginCallback = function(success, data)
     {
-        if(success && data.r == 'ok')
+        if(success && data.r == 'ok') {
             CMS.showUI();
-        else {
+        } else {
             $('#cms_login').dialog('open');
             $('#cms_login .error').html('Username or password incorrect.');
             $('#cms_login .ui-state-error').show();
@@ -307,7 +139,7 @@ var CMS = new Object();
                 d : data
             },
             success : cbSuccess,
-            error : cbError,
+            error : cbError
         });
     };
 
@@ -346,8 +178,9 @@ var CMS = new Object();
 
     CMS.pageOpen = function(page_id, no_change)
     {
-        if(typeof no_change == 'undefined')
+        if(typeof no_change == 'undefined') {
             $('#cms_page iframe').attr('src', 'page.php?id='+page_id);
+        }
         $('#CMS_Tree_Page_'+CMS.currentPage).css('font-weight', 'normal');
         CMS.currentPage = page_id;
         $('#CMS_Tree_Page_'+page_id).css('font-weight', 'bold');
@@ -359,10 +192,190 @@ var CMS = new Object();
             if(success && data.r == 'ok') {
                 $('#cms_page_new select').text('').append('<option value=""></option>');
                 for(var i in data.d) {
-                    $('#cms_page_new select').append('<option value="'+data.d[i]+'">'+data.d[i]+'</option>');
+                    if(data.d.hasOwnProperty(i)) {
+                        $('#cms_page_new select').append('<option value="'+data.d[i]+'">'+data.d[i]+'</option>');
+                    }
                 }
                 $('#cms_page_new').dialog('open');
             }
         });
     };
-};
+/*}*/
+
+$().ready(function() {
+
+    $('#cms_login').dialog({
+        autoOpen : false,
+        title : "Login",
+        buttons : {
+            "Go" : CMS.login
+        },
+        draggable : false,
+        resizable : false,
+        modal : true,
+        dialogClass : 'cms_login',
+        width : 250,
+        open : function(ev, ui) {
+            CMS.hideUI();
+            $(this).children('input').attr('value', '');
+            $(this).children('.focus').focus();
+        }
+    });
+    $('#cms_login input').keyup(function(ev) {
+        if(ev.keyCode == 13) {
+            CMS.login();
+        }
+    });
+
+    $('#cms_message').dialog({
+        autoOpen : false,
+        title : "Working...",
+        draggable : false,
+        resizable : false,
+        modal : true,
+        dialogClass : 'cms_message',
+        width: 200,
+        height: 100
+    });
+
+    $('#cms_tools').dialog({
+        autoOpen : false,
+        title : "Management",
+        resizable : false,
+        width : 'auto',
+        height : 'auto',
+        position : [ 20, 20 ],
+        dialogClass : 'cms_tools',
+        open : function(ev, ui) {
+            $('.cms_tools').trigger('mouseout');
+        }
+    });
+    $('.cms_tools').mouseover(function(ev) { $(this).fadeTo(0, 1); });
+    $('.cms_tools').mouseout(function(ev) { $(this).fadeTo(0, 0.7); });
+
+    $('#cms_page_new').dialog({
+        autoOpen : false,
+        title : "New page",
+        resizable : false,
+        width : 400,
+        height : 'auto',
+        buttons : {
+            "Cancel" : function(ev, ui) { $('#cms_page_new').dialog('close'); },
+            "Create" : function(ev, ui) {
+                var title = $('#cms_page_new input[name=title]').attr('value');
+                var template = $('#cms_page_new select[name=template]').val();
+                var parent_id = CMS.currentPage ? CMS.currentPage : null;
+
+                if(title.length === 0) {
+                    $('#cms_page_new .error').html("Title can't be empty.");
+                    $('#cms_page_new .ui-state-error').show();
+                    $('#cms_page_new .focus').focus();
+                    return;
+                } else if(template.length === 0) {
+                    $('#cms_page_new .error').html("You must select a template.");
+                    $('#cms_page_new .ui-state-error').show();
+                    $('#cms_page_new .focus').focus();
+                    return;
+                }
+
+                $('#cms_page_new').dialog('close');
+                CMS.query('page_new', { parent_id : parent_id, title : title, template : template }, CMS.updateTree);
+            }
+        },
+        open : function() {
+            var parent = CMS.currentPage;
+            if(parent === 0) {
+                parent = '<em>root</em>';
+            }
+            $('#cms_page_new span.parent').html(parent);
+            $(this).children('input[name=title]').attr('value', '');
+            $(this).children('.ui-state-error').hide();
+            $(this).children('.focus').focus();
+        }
+    });
+
+    $('#cms_label').dialog({
+        autoOpen : false,
+        title : "Label Editor",
+        resizable : true,
+        width : 400,
+        minHeight : 110,
+        height : 'auto',
+        modal : true,
+        dialogClass : 'cms_label',
+        buttons : {
+            "Cancel" : function(ev, ui) { $('#cms_label').dialog('close'); },
+            "Save" : function(ev, ui) {
+                $('#cms_label').dialog('close');
+                CMS.save($('#cms_label input[name=page_id]').attr('value'), $('#cms_label input[name=field]').attr('value'), $('#cms_label input[name=value]').attr('value'));
+            }
+        },
+        open : function() {
+            $(this).children('input[name=value]').focus();
+        }
+    });
+    $('#cms_label input[name=value]').keyup(function(ev) {
+        if(ev.keyCode == 13) {
+            $('#cms_label').dialog('close');
+            CMS.save($('#cms_label input[name=page_id]').attr('value'), $('#cms_label input[name=field]').attr('value'), $('#cms_label input[name=value]').attr('value'));
+        }
+    });
+
+    $('#cms_tinymce').dialog({
+        autoOpen : false,
+        title : "HTML Editor",
+        resizable : false,
+        width : 'auto',
+        height : 'auto',
+        modal : true,
+        minWidth : 800,
+        minHeight : 500,
+        dialogClass : 'cms_tinymce',
+        zIndex : 1,
+        buttons : {
+            "Cancel" : function(ev, ui) { $('#cms_tinymce').dialog('close'); },
+            "Save" : function(ev, ui) {
+                $('#cms_tinymce').dialog('close');
+                CMS.save($('#cms_tinymce input[name=page_id]').attr('value'), $('#cms_tinymce input[name=field]').attr('value'), $('#cms_tinymce textarea[name=value]').attr('value'));
+            }
+        },
+        open : function() {
+            $('#cms_tinymce textarea').tinymce({
+                    // Location of TinyMCE script
+                    script_url : 'js/tinymce/jscripts/tiny_mce/tiny_mce.js',
+
+                    // General options
+                    theme : "advanced",
+                    plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
+                    width: '800px',
+                    height: '450px',
+
+                    // Theme options
+                    theme_advanced_buttons1 : "newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
+                    theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+                    theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+                    theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
+                    theme_advanced_toolbar_location : "top",
+                    theme_advanced_toolbar_align : "left",
+                    theme_advanced_statusbar_location : "bottom",
+                    theme_advanced_resizing : true
+            });
+        },
+        close : function() {
+            $('#cms_tinymce textarea').tinymce().remove();
+        }
+    });
+
+    $('#cms_page iframe').load(function() {
+        var uri = $('#cms_page iframe')[0].contentWindow.location.href;
+        if(uri) {
+            var m = uri.match(/id=(\d+)$/);
+            if(m) {
+                CMS.pageOpen(m[1], true);
+            }
+        }
+    });
+
+    CMS.checkSession();
+});
+
